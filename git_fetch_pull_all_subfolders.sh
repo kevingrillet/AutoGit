4e4f0c27d9234cfb8b07645e5579f00efb53f95a
Kévin GRILLET <k.grillet@ymag.fr>
2021-03-22 12:11:43 +0100

    Update git_fetch_pull_all_subfolders.sh

--- git_fetch_pull_all_subfolders.sh
+++ git_fetch_pull_all_subfolders.sh
@@ -6,7 +6,7 @@
 #
 #   Regarde si les sous-dossiers sont des repo.
 #   Si oui: fetch puis pull si pas de modification en attente.
-# 
+#
 # Exemples:
 #   Placer le fichier dans [PATH] et double clic dessus.
 #
@@ -41,17 +41,23 @@ do
     if [ -d "$REPOSITORIES/$REPO/.git" ]
     then
       cd "$REPOSITORIES/$REPO"
-      echo -e "${GREEN}git fetch --all --prune${NC}"
-      git fetch --all --prune
-      echo -e "${GREEN}git status${NC}"
-      git status
-      if [[ -z "$(git status --porcelain)" ]]
+      echo -e "${GREEN}git fetch --all --prune --prune-tags${NC}"
+      git fetch --all --prune --prune-tags
+      CURRENT_BRANCH=`git branch --show-current`
+      if [[ -n "$(git ls-remote origin ${CURRENT_BRANCH})" ]]
       then
-        echo -e "${GREEN}git pull${NC}"
-        git pull
+        echo -e "${GREEN}git status${NC}"
+        git status
+        if [[ -z "$(git status --porcelain)" ]]
+        then
+          echo -e "${GREEN}git pull${NC}"
+          git pull
+        fi
+        echo -e "${GREEN}git describe --tag${NC}"
+        git describe --tag
+      else
+        echo -e "${YELLOW}Skipping because it doesn't look like it has a remote branch.${NC}"
       fi
-      echo -e "${GREEN}git describe --tag${NC}"
-      git describe --tag
     else
       echo -e "${YELLOW}Skipping because it doesn't look like it has a .git folder.${NC}"
     fi
