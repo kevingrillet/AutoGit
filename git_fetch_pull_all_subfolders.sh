57058220636c1594e89e5c351d20264a2fcae2c3
Kévin GRILLET <k.grillet@ymag.fr>
2021-03-25 11:08:46 +0100

    Update git_fetch_pull_all_subfolders.sh

--- git_fetch_pull_all_subfolders.sh
+++ git_fetch_pull_all_subfolders.sh
@@ -61,7 +61,7 @@ do
     else
       echo -e "${YELLOW}Skipping because it doesn't look like it has a .git folder.${NC}"
     fi
-    echo -e "${BLUE}Done at ${RED}`date`${NC}"
+    echo -e "${BLUE}Done ${WHITE}$REPOSITORIES/$REPO${BLUE} at ${RED}`date`${NC}"
     echo
   fi
 done
